0cd2c5b34140cab6a1bae51f560ecd4f5741807a
victor <eduardopontesdias@outlook.com>
2023-08-14 20:36:53 -0300

    Front do Projeto

---
+++ js/meu_script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//desabilita o botão no início
+document.getElementById("botao").disabled = true;
+
+//cria um event listener que escuta mudanças no input
+document.getElementById("input").addEventListener("input", function(event){
+
+  //busca conteúdo do input
+    var conteudo = document.getElementById("input").value;
+
+    //valida conteudo do input 
+    if (conteudo !== null && conteudo !== '') {
+      //habilita o botão
+      document.getElementById("botao").disabled = false;
+    } else {
+      //desabilita o botão se o conteúdo do input ficar em branco
+      document.getElementById("botao").disabled = true;
+    }
+});
